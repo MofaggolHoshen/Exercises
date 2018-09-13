0590a07ea0fdbd2683652dbd58a5aafba40791f9
Mofaggol Hoshen <mofaggol.hoshen@quipu.de>
2018-09-13 12:39:43 +0200

    Added Quantifiers linq

---
+++ LinqExercises/Quantifiers/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.ComponentModel;
+using LinqCommon;
+
+namespace Quantifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqExamples samples = new LinqExamples();
+
+
+            samples.LinqAny01();
+            samples.LinqAny02();
+
+            samples.LinqAll01();
+            samples.LinqAll02();
+
+            samples.LinqContains01();
+            samples.LinqContains02();
+
+        }
+        
+        class LinqExamples
+        {
+            public void LinqAny01()
+            {
+                string[] words = { "believe", "relief", "receipt", "field" };
+
+                bool iAfterE = words.Any(w => w.Contains("ei"));
+                
+                Console.WriteLine("There is a word in the list that contains 'ei': {0}", iAfterE);
+            }
+
+            
+            public void LinqAny02()
+            {
+                List<Product> products = LinqHellper.GetProducts();
+
+                var productGroups =
+                    from prod in products
+                    group prod by prod.Category into prodGroup
+                    where prodGroup.Any(p => p.UnitsInStock == 0)
+                    select new { Category = prodGroup.Key, Products = prodGroup };
+            }
+            
+            public void LinqAll01()
+            {
+                int[] numbers = { 1, 11, 3, 19, 41, 65, 19 };
+
+                bool onlyOdd = numbers.All(n => n % 2 == 1);
+
+                Console.WriteLine("The list contains only odd numbers: {0}", onlyOdd);
+            }
+            
+            public void LinqAll02()
+            {
+                List<Product> products = LinqHellper.GetProducts();
+
+                var productGroups =
+                    from prod in products
+                    group prod by prod.Category into prodGroup
+                    where prodGroup.All(p => p.UnitsInStock > 0)
+                    select new { Category = prodGroup.Key, Products = prodGroup };
+            }
+
+            public void LinqContains01()
+            {
+
+                string[] fruits = { "apple", "banana", "mango", "orange", "passionfruit", "grape" };
+
+                string fruit = "mango";
+
+                bool hasMango = fruits.Contains(fruit);
+
+                Console.WriteLine(
+                    "The array {0} contain '{1}'.",
+                    hasMango ? "does" : "does not",
+                    fruit);
+            }
+
+            /// <summary>
+            /// public static bool Contains<TSource> (this System.Collections.Generic.IEnumerable<TSource> source, TSource value, System.Collections.Generic.IEqualityComparer<TSource> comparer);
+            /// </summary>
+            public void LinqContains02()
+            {
+
+                Fruit[] fruits = { new Fruit { Name = "apple", Code = 9 },
+                       new Fruit { Name = "orange", Code = 4 },
+                       new Fruit { Name = "lemon", Code = 12 } };
+
+                Fruit apple = new Fruit { Name = "apple", Code = 9 };
+                Fruit kiwi = new Fruit { Name = "kiwi", Code = 8 };
+
+                FruitComparer prodc = new FruitComparer();
+
+                bool hasApple = fruits.Contains(apple, prodc);
+                bool hasKiwi = fruits.Contains(kiwi, prodc);
+
+                Console.WriteLine("Apple? " + hasApple);
+                Console.WriteLine("Kiwi? " + hasKiwi);
+            }
+        }
+    }
+
+    public class Fruit
+    {
+        public string Name { get; set; }
+        public int Code { get; set; }
+    }
+    
+    //Copied from https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.contains?view=netframework-4.7.2
+    class FruitComparer : IEqualityComparer<Fruit>
+    {
+        // Products are equal if their names and product numbers are equal.
+        public bool Equals(Fruit x, Fruit y)
+        {
+
+            //Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(x, y)) return true;
+
+            //Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+
+            //Check whether the products' properties are equal.
+            return x.Code == y.Code && x.Name == y.Name;
+        }
+
+        // If Equals() returns true for a pair of objects 
+        // then GetHashCode() must return the same value for these objects.
+
+        public int GetHashCode(Fruit product)
+        {
+            //Check whether the object is null
+            if (Object.ReferenceEquals(product, null)) return 0;
+
+            //Get hash code for the Name field if it is not null.
+            int hashProductName = product.Name == null ? 0 : product.Name.GetHashCode();
+
+            //Get hash code for the Code field.
+            int hashProductCode = product.Code.GetHashCode();
+
+            //Calculate the hash code for the product.
+            return hashProductName ^ hashProductCode;
+        }
+
+    }
+}
---
+++ LinqExercises/Quantifiers/README.md
@@ -0,0 +1,32 @@
+# Restriction Operators
+
+### Any
+The Any operator checks whether any element of a sequence satisfies a condition.
+
+The Any operator enumerates the source sequence and returns true if any element satisfies the test given by the predicate. If no predicate function 
+is specified, the Any operator simply returns true if the source sequence contains any elements.
+
+##### Remarks
+
+This method does not return any one element of a collection. Instead, it determines whether the collection contains any elements.
+The enumeration of source is stopped as soon as the result can be determined.  
+
+### All
+The All operator checks whether all elements of a sequence satisfy a condition.
+
+##### Remarks
+
+This method does not return all the elements of a collection. Instead, it determines whether all the elements of a collection satisfy a condition.
+The enumeration of source is stopped as soon as the result can be determined
+
+### Contains
+The Contains operator checks whether a sequence contains a given element.
+
+##### Remarks
+
+If the type of source implements ICollection<T>, the Contains method in that implementation is invoked to obtain the result. Otherwise, 
+this method determines whether source contains the specified element.
+Enumeration is terminated as soon as a matching element is found. 
+
+More details : 
+1. https://msdn.microsoft.com/en-us/library/bb394939.aspx#standardqueryops_topic15
