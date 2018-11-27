37945456acd29862d4c490a9fa79bac5153d2966
summoncse <summoncse@gmail.com>
2018-11-27 17:34:27 +0100

    Async programing

---
+++ VariousExcercises/VariousExcercises/AsyncProgramming.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariousExcercises
+{
+    [TestClass]
+    public class AsyncProgramming
+    {
+
+        public async Task GetLongTermAsync()
+        {
+            Debug.WriteLine("GetLongTermAsync enter.");
+
+            await Task.Delay(20000);
+
+            Debug.WriteLine("GetLongTermAsync, i am done.");
+
+        }
+
+        public async Task<string> GetTaskAsync()
+        {
+            Debug.WriteLine("GetTaskAsync entered.");
+
+            await Task.Delay(20000);
+
+            Debug.WriteLine("GetTaskAsync, i am returning.");
+
+            return "mofaggol";
+        }
+
+        public async Task TestHeperMethod()
+        {
+            Debug.WriteLine("GetLongTermAsync started.");
+
+            await GetLongTermAsync();
+
+            Debug.WriteLine("GetLongTermAsync finished.");
+
+            Debug.WriteLine("GetLongTermAsync started.");
+
+            var result = await GetTaskAsync();
+
+            Debug.WriteLine("GetLongTermAsync finished.");
+
+        }
+
+        [TestMethod]
+        public async Task AsyncProgramTest01()
+        {
+
+            await TestHeperMethod();
+        }
+    }
+}
