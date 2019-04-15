ffdd067fc17ad27ec9f49f14de797f69795f25bb
summoncse <summoncse@gmail.com>
2019-04-15 17:08:15 +0200

    Previous page example

--- VariousExcercises/CoreSample/Controllers/SumitGoBackController.cs
+++ VariousExcercises/CoreSample/Controllers/SumitGoBackController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using FiltersSample.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FiltersSample.Controllers
@@ -10,6 +12,27 @@ namespace FiltersSample.Controllers
     {
         public IActionResult Index()
         {
+            var model = new Person
+            {
+                FirstName = "Mofaggol",
+                LastName = "Hoshen",
+                Address = new Address
+                {
+                    City = "Frankfurt am Main",
+                    Country = "Germany"
+                }
+            };
+
+            return View(model);
+        }
+
+        public IActionResult SaveNgoBack(Person arg)
+        {
+            var j = arg;
+
+            if (Request.Headers.Any(i => i.Key == "Referer"))
+                return Redirect(Request.Headers["Referer"].ToString());
+
             return View();
         }
     }
