531b9acbd9cc8ce45c66851abfc1770206c90978
summoncse <summoncse@gmail.com>
2019-06-14 17:14:13 +0200

    IValidationObject sample

---
+++ VariousExcercises/CoreSample/Controllers/UploadFileController.cs
@@ -0,0 +1,27 @@
+﻿using CoreSample.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSample.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Photo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Photo(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+            return View(userViewModel);
+        }
+    }
+}
