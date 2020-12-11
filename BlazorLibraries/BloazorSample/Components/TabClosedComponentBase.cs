4194eeb41492a6a7afa5d1a690e155f42b9d34ac
summoncse <summoncse@gmail.com>
2020-12-11 10:37:54 +0100

    Updated

---
+++ BlazorLibraries/BloazorSample/Components/TabClosedComponentBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloazorSample.Components
+{
+    public class TabClosedComponentBase : ComponentBase, IDisposable
+    {
+        public void Dispose()
+        {
+            
+        }
+    }
+}
